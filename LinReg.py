3d24b90f12d98b2955649c0f4e3d91bd64bbd09d
SherinGr <sheringrimbergen@hotmail.com>
2019-09-27 16:31:53 +0200

    Guru99 finished up to tutorial 10. Skipped tutorials 9 and 10, because too much theoretical about pattern recognition.
    Signed-off-by: SherinGr <sheringrimbergen@hotmail.com>

--- LinReg.py
+++ LinReg.py
@@ -54,7 +54,56 @@ data_char = train['medv'].describe()
 pred = estimator.predict(input_fn=get_input_fn(predict, num_epochs=1, n_batch=128, shuffle=False))
 
 """ Same Solution with NUMPY """
+# convert data to numerical values:
+train_np = train.values
+test_np = test.values
+predict_np = predict.values
 
 
-def prep_data(dataFrame):
-    pass
+def prep_data(data_np):
+    """Separate labels and features"""
+    feat = data_np[:, :-1]
+    lab = data_np[:, -1]
+    return feat, lab
+
+
+train_feat, train_lab = prep_data(train_np)
+test_feat, test_lab = prep_data(test_np)
+predict_feat = predict_np[:, :-1]  # exclude label column
+
+feature_cols = [tf.feature_column.numeric_column('x', shape=train_feat.shape[1:])]
+# note train_feat.shape[1] does not yield a tuple! This statement defines that the
+# data has a single feature with dimension 9, as is our numpy data. Note that with
+# pandas we have a nine-dimensional feature_cols object!
+
+estimator = tf.estimator.LinearRegressor(feature_columns=feature_cols,
+                                         model_dir="train1")
+
+# This inputs all features together in the x dictionary, whereas in pandas
+# all features are separated in the x dict.
+train_input = tf.estimator.inputs.numpy_input_fn(x={'x': train_feat},
+                                                 y=train_lab,
+                                                 batch_size=128,
+                                                 shuffle=False,
+                                                 num_epochs=None)
+
+eval_input = tf.estimator.inputs.numpy_input_fn(x={'x':test_feat},
+                                                y=test_lab,
+                                                shuffle=False,
+                                                batch_size=128,
+                                                num_epochs=1)
+
+estimator.train(input_fn=train_input, steps=5000)
+
+result2 = estimator.evaluate(input_fn=eval_input, steps=None)
+
+test_input = tf.estimator.inputs.numpy_input_fn(
+        x={'x': predict_feat},
+        batch_size=128,
+        num_epochs=1,
+        shuffle=False)
+
+y = estimator.predict(test_input)
+
+# TODO: I do not understand this at all:
+predictions = list(p['predictions'] for p in itertools.islice(y, 2))
