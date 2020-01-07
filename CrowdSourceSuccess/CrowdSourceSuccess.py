fc28da7b60b94ed389696b8cfb1530d893a5c42d
SherinGr <sheringrimbergen@hotmail.com>
2020-01-07 17:34:23 +0100

    Added linearSVC classifier, 90% accuracy with 2 features.
    Signed-off-by: SherinGr <sheringrimbergen@hotmail.com>

--- CrowdSourceSuccess/CrowdSourceSuccess.py
+++ CrowdSourceSuccess/CrowdSourceSuccess.py
@@ -31,9 +31,6 @@ data = data.drop(features[0], axis=1)
 
 ''' Lets inspect the data with some plots '''
 
-
-f, axes = plt.subplots(1, 2)
-
 #sns.distplot(data.usd_pledged)
 #sns.distplot(data.backers_count)
 
@@ -57,14 +54,15 @@ print(data.backers_count.describe())
 # seems the data is very skewed, apply minmax after capping off the data to maintain spread!
 
 # To check categorical features:
-print(data.category.unique())  # around 15
-print(data.subcategory.unique())  # a lot! is it usefull?
+#print(data.category.unique())  # around 15
+#print(data.subcategory.unique())  # a lot! is it usefull?
 
 """ The Classifier """
 
 # We will make a tree based classifier
 # First, we can already achieve 90% accuracy by using a naive Bayes classifier on conv_pledged vs goal.
 from sklearn import svm
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 clf = svm.LinearSVC()
@@ -74,31 +72,37 @@ data_cleaned = data[['converted_pledged_amount', 'goal']]
 # SVM assumes: mean zero same order variance! Transform data!
 
 scaler.fit_transform(data_cleaned)
-data_transformed = scaler.transform(data_cleaned)
+print('Scaling done')
+data_transformed = data_cleaned #scaler.transform(data_cleaned)
+
+train, test = train_test_split(data_transformed, labels, test_size=0.3, shuffle=True)
 
 df = pd.DataFrame(data_transformed, columns=['converted_pledged_amount', 'goal'])
 clf.fit(data_transformed, labels)
+print('Training done')
+print(clf.score(data_cleaned, labels))
+
+# Plot the classifier:
+f, ax = plt.subplots()
 
 plot_sample = df.sample(100)
 sample_labs = labels[plot_sample.index]
 sns.scatterplot(x=plot_sample.goal,
                 y=plot_sample.converted_pledged_amount,
                 hue=sample_labs,
-                ax=axes[0])
+                ax=ax)
 
-print(clf.score(data_cleaned, labels))
-
-
-# Plot the classifier:
-ax = plt.gca() # get current axes
 xlim = ax.get_xlim()
 ylim = ax.get_ylim()
 
-xx, yy = np.meshgrid(np.linspace(xlim[0], xlim[1], 25),
-                     np.linspace(ylim[0], ylim[1], 25))
+xx, yy = np.meshgrid(np.linspace(xlim[0], xlim[1], 50),
+                     np.linspace(ylim[0], ylim[1], 50))
 
-z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 z = z.reshape(xx.shape)
 
-plt.plot(xx,yy,z ax=ax)
+ax.contourf(xx, yy, z, alpha=0.8)
 
+# Already 91% accuracy (?)
+
+# Better turn this into a new feature, subtract the two, make it binary <0 or >0.
